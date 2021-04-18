def25c2f18bc4b21e07005f678fab68600dd22ec
PC <PC@DESKTOP-C1QKJRI>
2021-04-18 19:42:21 +0900

    0418관리자 탭메뉴 작업중/img폴더 서브로고 추가

---
+++ Arbor/src/main/java/com/arbor/home/member/MemberController.java
@@ -0,0 +1,21 @@
+package com.arbor.home.member;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	@RequestMapping("/memberSearch")
+	public String membersearch() {
+		return "member/memberSearch";
+	}
+	
+	
+	
+}
