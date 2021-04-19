6a56d42b24b603688b514347456d6a323016f9b8
bit <bit@DESKTOP-MEMCFP1>
2021-04-19 14:42:50 +0900

    2021-04-19 무서웡두려웡

--- Arbor/src/main/java/com/arbor/home/product/ProductController.java
+++ Arbor/src/main/java/com/arbor/home/product/ProductController.java
@@ -5,10 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
+	
+	@RequestMapping("/productList")
+	public String productList() {
+		return "product/productList";
+	}
 
 	@RequestMapping("/productInsert")
 	public String productInsert() {
 		return "product/productInsert";
+	}
+	
+	@RequestMapping("/productView")
+	public String productView() {
+		return "product/productView";
 	}
 
 }
