7b80f2ec2e47b62e80e393807dea716b0473a88d
lee <lee@DESKTOP-6AP8C14>
2021-04-26 19:10:52 +0900

    mypage 결제내역 개발중

--- Arbor/src/main/java/com/arbor/home/myPage/MyPageController.java
+++ Arbor/src/main/java/com/arbor/home/myPage/MyPageController.java
@@ -1,12 +1,20 @@
 package com.arbor.home.myPage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.arbor.home.PageSearchVO;
+import com.arbor.home.orderTbl.OrderTblVO;
+import com.arbor.home.subOrder.SubOrderVO;
 
 @Controller
 public class MyPageController {
@@ -14,12 +22,35 @@ public class MyPageController {
 	SqlSession sqlSession;
 	
 	//마이페이지 구매내역
-	@RequestMapping("purchaseList")
+	@RequestMapping("/purchaseList")
 	public ModelAndView purchaseList(HttpServletRequest req) {
+		String pageNumStr = req.getParameter("pageNum");
+		MyPageDAOImp dao = sqlSession.getMapper(MyPageDAOImp.class);
 		ModelAndView mav = new ModelAndView();
+		PageSearchVO pageVo = new PageSearchVO();
+		List<OrderTblVO> orderVo = dao.purchaseList(pageVo);
+		
+		pageVo.setTotalRecord(dao.totalRecord(pageVo));
+		
+		if(pageNumStr != null) {
+			pageVo.setPageNum(Integer.parseInt(pageNumStr));
+		}
+		
+		mav.addObject("list", orderVo);
+		mav.addObject("pageVO", pageVo);
 		
 		mav.setViewName("client/myPage/myPageMain");
 		return mav;
+	}
+	//구매내역 상품상페 팝업
+	@RequestMapping("/orderPopup")
+	@ResponseBody
+	public List<SubOrderVO> orderPopup(int orderno) {
+		List<SubOrderVO> list = new ArrayList<SubOrderVO>();
+		MyPageDAOImp dao = sqlSession.getMapper(MyPageDAOImp.class);
+		
+		
+		return list;
 	}
 	
 }
